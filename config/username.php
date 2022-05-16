2d24743a50e37848b9a04ef8bdd56f5eb7b3508c
Hans Rio <hansolo.palla2115@gmail.com>
2022-05-16 20:15:23 +0800

    Memperindah tampilan dashboard dan memperpendek sintaks

---
+++ config/username.php
@@ -0,0 +1,4 @@
+<a href="profile.html" class="flex items-center">
+    <img src="https://source.unsplash.com/1080x1080?profile" alt="Profile" width="36" class="rounded-full">
+    <span class="ml-4 font-bold underline">Wayan Micheal Jonathan Suarno</span>
+</a>
--- kategori.php
+++ kategori.php
@@ -4,17 +4,14 @@
         <?php include 'header.php'; ?>
         <title>EdgeStore - Kategori</title>
     </head>
-    <body class="bg-white  font-[Inter] flex">
+    <body class="font-[Inter] flex">
         <!-- Sidebar -->
         <?php include 'sidebar.php'; ?>
         <div class="content flex basis-11/12 bg-indigo-200 h-screen duration-1000">
             <div class="bg-white min-w-full my-10 overflow-auto">
                 <div class="flex mx-12 mt-12">
                     <h1 class="grow text-4xl text-slate-700 font-bold ">Kategori</h1>
-                    <a href="profile.html" class="flex items-center">
-                        <img src="https://source.unsplash.com/1080x1080?profile" alt="Profile" width="36" class="rounded-full">
-                        <span class="ml-4 font-bold">Wayan Micheal Jonathan Suarno</span>
-                    </a>
+                    <?php include 'config/username.php'; ?>
                 </div>
                 
             </div>
