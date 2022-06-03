a66f6a271ff6c5576ad07e439f65ab9ff34aaa19
Hans Rio <hansolo.palla2115@gmail.com>
2022-06-03 23:20:16 +0800

    Mengupdate fitur keranjang

--- ajax/tambahKeranjang.php
+++ ajax/tambahKeranjang.php
@@ -1,21 +1,52 @@
 <?php
     require '../config/functions.php';
 
-    $item_id = $_GET["item"];
+    // if( isset($_POST["total"]) ) {
+    //     $total = ;
+    // }
 
-    $jumlah = 1;
-    $total = 100000;
+    $banyak_id = $_GET["items"];
+    $banyak_item = $_GET["length"];
+    $items_id = array();
+    $temp = "";
+
+    // echo $banyak_id;
+    // $jumlah = 1;
+    // $total = 100000;
+
+    for ($i = 0; $i < strlen($banyak_id); $i++) {
+        
+        if($banyak_id[$i] != ",")
+            $temp .= "$banyak_id[$i]";
+        else{
+            array_push($items_id, $temp);
+            $temp = "";
+        }
+    }
+
+    // print_r($items_id);
+    
     $select_query = "
         SELECT 
             tb_barang.id_barang, 
             tb_barang.nama_barang, 
             tb_barang.merk,
-            tb_barang.harga_jual
+            tb_barang.harga_jual,
+            tb_barang.stok
         FROM tb_barang 
         WHERE
-            tb_barang.id_barang = $item_id;
+            tb_barang.id_barang IN ($items_id[0]
         ";
     
+    if($banyak_item > 1){
+        for($i = 1; $i < $banyak_item; $i++){
+            $select_query = $select_query .= " ,$items_id[$i]";
+        }
+    }
+
+    $select_query = $select_query .= ")";
+    // var_dump($select_query);
+
     // $insert_query = "INSERT INTO tb_transaksi(id_barang, jumlah, total, kasir) VALUES ('$item_id','$jumlah','$total','$kasir')";
     $addKeranjang = query($select_query);
 ?>
@@ -27,7 +58,6 @@
         <th>Merk</th>
         <th>Jumlah</th>
         <th>Total</th>
-        <th>Aksi</th>
     </tr>
     
     <?php $i = 1; ?>
@@ -36,9 +66,17 @@
         <td><?= $i; ?></td>
         <td><?= $row["nama_barang"]; ?></td>
         <td><?= $row["merk"]; ?></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>
+            <form action="" method="post">
+                <?php $total = $row["harga_jual"] * 1; ?> 
+                <select name="total" onchange="this.form.submit()" class="border-2 border-slate-600 p-2 rounded-lg">
+                <?php for($i = 0; $i < $row["stok"]; $i++){ ?>
+                    <option value="<?= $i + 1 ?>"><?= $i + 1 ?></option>
+                <?php } ?>
+                </select>
+            </form>
+        </td>
+        <td><?= $total; ?></td>
     </tr>
     <?php $i++; ?>
     <?php endforeach; ?>
